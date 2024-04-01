bb98f6f14ecae865e7d149d36c5de5996bc46d43
arijit-s98 <sarijit2172@gmail.com>
2024-04-01 20:08:13 +0530

    added events

---
+++ Basics/Project 7- ToDo App/index.js
@@ -0,0 +1,49 @@
+console.log("Welcome to my todo app")
+
+let todoDataSection = document.getElementById('todo-data')
+function addTodo() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let getTodosButton = document.getElementById('get-todos');
+
+// registration of event listener
+
+// getTodosButton.addEventListener("click", () => {
+//     console.log("clicked");
+// });
+
+// different ways to add events
+
+// getTodosButton.onclick = () => {
+//     console.log('clicked');
+// }
+
